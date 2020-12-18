3abad11a59a6d382586600115fefc8df63ab1a3e
Neal Kruis <neal.kruis@bigladdersoftware.com>
2020-12-18 09:51:57 -0700

    Add new 'to' units function.

--- resdx/models.py
+++ resdx/models.py
@@ -2,7 +2,7 @@ from scipy import optimize
 
 from .util import calc_biquad, calc_quad
 from .psychrometrics import psychrolib
-from .units import u, convert
+from .units import fr_u, to_u
 
 from .defrost import Defrost, DefrostControl, DefrostStrategy
 
@@ -12,8 +12,8 @@ from .defrost import Defrost, DefrostControl, DefrostStrategy
 '''Based on Cutler et al, but also includes internal EnergyPlus calculations'''
 
 def cutler_cooling_power(conditions, power_scalar):
-  T_iwb = convert(conditions.indoor.get_wb(),"K","°F") # Cutler curves use °F
-  T_odb = convert(conditions.outdoor.db,"K","°F") # Cutler curves use °F
+  T_iwb = to_u(conditions.indoor.get_wb(),"°F") # Cutler curves use °F
+  T_odb = to_u(conditions.outdoor.db,"°F") # Cutler curves use °F
   eir_FT = calc_biquad([-3.437356399, 0.136656369, -0.001049231, -0.0079378, 0.000185435, -0.0001441], T_iwb, T_odb)
   eir_FF = calc_quad([1.143487507, -0.13943972, -0.004047787], conditions.air_mass_flow_fraction)
   cap_FT = calc_biquad([3.68637657, -0.098352478, 0.000956357, 0.005838141, -0.0000127, -0.000131702], T_iwb, T_odb)
@@ -21,15 +21,15 @@ def cutler_cooling_power(conditions, power_scalar):
   return eir_FF*cap_FF*eir_FT*cap_FT*power_scalar
 
 def cutler_total_cooling_capacity(conditions, capacity_scalar):
-  T_iwb = convert(conditions.indoor.get_wb(),"K","°F") # Cutler curves use °F
-  T_odb = convert(conditions.outdoor.db,"K","°F") # Cutler curves use °F
+  T_iwb = to_u(conditions.indoor.get_wb(),"°F") # Cutler curves use °F
+  T_odb = to_u(conditions.outdoor.db,"°F") # Cutler curves use °F
   cap_FT = calc_biquad([3.68637657, -0.098352478, 0.000956357, 0.005838141, -0.0000127, -0.000131702], T_iwb, T_odb)
   cap_FF = calc_quad([0.718664047, 0.41797409, -0.136638137], conditions.air_mass_flow_fraction)
   return cap_FF*cap_FT*capacity_scalar
 
 def cutler_steady_state_heating_power(conditions, power_scalar):
-  T_idb = convert(conditions.indoor.db,"°K","°F") # Cutler curves use °F
-  T_odb = convert(conditions.outdoor.db,"K","°F") # Cutler curves use °F
+  T_idb = to_u(conditions.indoor.db,"°F") # Cutler curves use °F
+  T_odb = to_u(conditions.outdoor.db,"°F") # Cutler curves use °F
   eir_FT = calc_biquad([0.718398423,0.003498178, 0.000142202, -0.005724331, 0.00014085, -0.000215321], T_idb, T_odb)
   eir_FF = calc_quad([2.185418751, -1.942827919, 0.757409168], conditions.air_mass_flow_fraction)
   cap_FT = calc_biquad([0.566333415, -0.000744164, -0.0000103, 0.009414634, 0.0000506, -0.00000675], T_idb, T_odb)
@@ -37,8 +37,8 @@ def cutler_steady_state_heating_power(conditions, power_scalar):
   return eir_FF*cap_FF*eir_FT*cap_FT*power_scalar
 
 def cutler_steady_state_heating_capacity(conditions, capacity_scalar):
-  T_idb = convert(conditions.indoor.db,"°K","°F") # Cutler curves use °F
-  T_odb = convert(conditions.outdoor.db,"K","°F") # Cutler curves use °F
+  T_idb = to_u(conditions.indoor.db,"°F") # Cutler curves use °F
+  T_odb = to_u(conditions.outdoor.db,"°F") # Cutler curves use °F
   cap_FT = calc_biquad([0.566333415, -0.000744164, -0.0000103, 0.009414634, 0.0000506, -0.00000675], T_idb, T_odb)
   cap_FF = calc_quad([0.694045465, 0.474207981, -0.168253446], conditions.air_mass_flow_fraction)
   return cap_FF*cap_FT*capacity_scalar
@@ -53,7 +53,7 @@ def epri_integrated_heating_capacity(conditions, capacity_scalar, defrost):
         heating_capacity_multiplier = 0.875*(1 - t_defrost)
 
     if defrost.strategy == DefrostStrategy.REVERSE_CYCLE:
-        Q_defrost_indoor_u = 0.01*(7.222 - convert(conditions.outdoor.db,"°K","°C"))*(capacity_scalar/1.01667)
+        Q_defrost_indoor_u = 0.01*(7.222 - to_u(conditions.outdoor.db,"°C"))*(capacity_scalar/1.01667)
     else:
         Q_defrost_indoor_u = 0
 
@@ -72,7 +72,7 @@ def epri_integrated_heating_power(conditions, power_scalar, capacity_scalar, def
       input_power_multiplier = 0.954*(1 - t_defrost)
 
     if defrost.strategy == DefrostStrategy.REVERSE_CYCLE:
-      T_iwb = convert(conditions.indoor.wb,"K","°C")
+      T_iwb = to_u(conditions.indoor.wb,"°C")
       T_odb = conditions.outdoor.db_C
       defEIRfT = calc_biquad([0.1528, 0, 0, 0, 0, 0], T_iwb, T_odb) # TODO: Check assumption from BEopt
       P_defrost = defEIRfT*(capacity_scalar/1.01667)
@@ -90,7 +90,7 @@ def epri_defrost_time_fraction(conditions):
 
 def coil_diff_outdoor_air_humidity(conditions):
   # EPRI algorithm as described in EnergyPlus documentation
-  T_coil_outdoor = 0.82 * convert(conditions.outdoor.db,"°K","°C") - 8.589  # In C
+  T_coil_outdoor = 0.82 * to_u(conditions.outdoor.db,"°C") - 8.589  # In C
   saturated_air_himidity_ratio = psychrolib.GetSatHumRatio(T_coil_outdoor,conditions.outdoor.p) # pressure in Pa already
   humidity_diff = conditions.outdoor.get_hr() - saturated_air_himidity_ratio
   return max(1.0e-6, humidity_diff)
@@ -125,10 +125,10 @@ def CA_regression(coeffs,T_ewb,T_odb,T_edb,V_std_per_rated_cap):
     coeffs[11]
 
 def title24_shr(conditions):
-  T_iwb = convert(conditions.indoor.get_wb(),"K","°F") # Cutler curves use °F
-  T_odb = convert(conditions.outdoor.db,"K","°F") # Title 24 curves use °F
-  T_idb = convert(conditions.indoor.db,"K","°F") # Title 24 curves use °F
-  CFM_per_ton = convert(conditions.std_air_vol_flow_per_capacity,"m**3/W/s","cu_ft/min/ton_of_refrigeration")
+  T_iwb = to_u(conditions.indoor.get_wb(),"°F") # Cutler curves use °F
+  T_odb = to_u(conditions.outdoor.db,"°F") # Title 24 curves use °F
+  T_idb = to_u(conditions.indoor.db,"°F") # Title 24 curves use °F
+  CFM_per_ton = to_u(conditions.std_air_vol_flow_per_capacity,"cu_ft/min/ton_of_refrigeration")
   coeffs = [0.0242020,-0.0592153,0.0012651,0.0016375,0,0,0,-0.0000165,0,0.0002021,0,1.5085285]
   SHR = CA_regression(coeffs,T_iwb,T_odb,T_idb,CFM_per_ton)
   return min(1.0, SHR)
--- resdx/units.py
+++ resdx/units.py
@@ -1,9 +1,20 @@
 import pint # 0.15 or higher
 ureg = pint.UnitRegistry()
 
-def u(value,unit):
-  return ureg.Quantity(value, unit).to_base_units().magnitude
+def fr_u(value,from_units):
+  '''Convert from given units into base SI units for calculation'''
+  return ureg.Quantity(value, from_units).to_base_units().magnitude
+
+#def u(value,from_units):
+#  '''Convert from given units into base SI units for calculation (old function)'''
+#  return fr_u(value,from_units)
 
 def convert(value, from_units, to_units):
+  '''Convert from any units to another (of the same dimension)'''
   return ureg.Quantity(value, from_units).to(to_units).magnitude
+
+def to_u(value,to_units):
+  '''Convert from base SI units to any other units'''
+  base_units = ureg.Quantity(value, to_units).to_base_units().units
+  return ureg.Quantity(value, base_units).to(to_units).magnitude
 
