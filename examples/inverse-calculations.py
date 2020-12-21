882657daf5dcc5810b2f85323c6e1ac95390fbc1
Amine Lazrak <amine.lazrak@bigladdersoftware.com>
2020-12-21 12:37:47 -0700

    Add figure name as an argument and clean y-axis label.

--- examples/inverse-calculations.py
+++ examples/inverse-calculations.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-def plot(x, y, xlabel, ylabel):
+def plot(x, y, xlabel, ylabel,figure_name):
     plt.figure()
     plt.plot(x, y)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.savefig(f'output/{ylabel.replace(" ", "")}_vs_{xlabel.replace(" ", "")}.png')
+    plt.savefig(f'output/{figure_name}.png')
 
 # Cooling 
 
@@ -48,7 +48,7 @@ for seer in seer_range:
     #print(f"seer = {seer:.1f}, cop = {cop:.2f}, fan_eff = {resdx.convert(fan_efficacy(seer),'W/(m**3/s)','W/(cu_ft/min)'):.3f}, C_d = {c_d(seer):.3f}, converged = {solution.converged}, iter = {solution.iterations}")
     cops_from_seer.append(cop)
 
-plot(seer_range, cops_from_seer, "SEER", "Gross (fan not included) COP (at A conditions)")
+plot(seer_range, cops_from_seer, "SEER", "Gross COP (at A conditions)","cooling-cop-v-seer")
 # seer = 13.0, cop = 3.72, fan_eff = 0.250, C_d = 0.100,
 #%% Heating inverse calculations
 hspf_range = np.arange(5,14,0.1)
@@ -59,5 +59,5 @@ for hspf in hspf_range:
     #print(f"hspf = {hspf:.1f}, cop = {cop:.2f}, fan_eff = {resdx.convert(fan_efficacy(estimated_seer(hspf)),'W/(m**3/s)','W/(cu_ft/min)'):.3f}, C_d = {c_d(estimated_seer(hspf)):.3f},converged = {solution.converged}, iter = {solution.iterations}")
     cops_from_hspf.append(cop)
 
-plot(hspf_range, cops_from_hspf, "HSPF", "Gross (fan not included) COP (at H1 conditions)")
+plot(hspf_range, cops_from_hspf, "HSPF", "Gross COP (at H1 conditions)","heating-cop-v-hspf")
 # hspf = 7.7, cop = 3.88, fan_eff = 0.250, C_d = 0.142
