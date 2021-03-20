885fc3c037a54876ea9a8ee4b102402554a4e8cf
Amine Lazrak <amine.lazrak@bigladdersoftware.com>
2021-03-20 10:46:56 -0600

    Update dew point variables names and add script to check for unrealistic air conditions.

--- resdx/psychrometrics.py
+++ resdx/psychrometrics.py
@@ -14,21 +14,35 @@ class PsychState:
     self.rh_set = False
     self.hr_set = False
     self.h_set = False
-    self.dewp_set = False
+    self.dp_set = False
     self.rho_set = False
     self.C_p = fr_u(1.006,"kJ/kg/K")
     if len(kwargs) > 1:
       sys.exit(f'only 1 can be provided')
     if "wetbulb" in kwargs:
       self.set_wb(kwargs["wetbulb"])
+      if (self.wb_C > self.db_C) or (self.wb_C < psychrolib.GetTWetBulbFromHumRatio(self.db_C, 0, self.p)):
+        sys.exit(f"Air cannot exist at these conditions: drybulb = {self.db_C}C and wetbulb = {self.wb_C}C")
     elif "hum_rat" in kwargs:
       self.set_hr(kwargs["hum_rat"])
+      hr_check = psychrolib.GetHumRatioFromTWetBulb(self.db_C, self.wb_C, self.p)
+      if abs(hr_check - self.hr)/hr_check > 0.01:
+        sys.exit(f"Air cannot exist at these conditions: drybulb = {self.db_C}C and humidity ratio = {self.hr}")
     elif "rel_hum" in kwargs:
       self.set_rh(kwargs["rel_hum"])
+      rh_check = psychrolib.GetRelHumFromTWetBulb(self.db_C, self.wb_C, self.p)
+      if abs(rh_check - self.rh)/rh_check > 0.01:
+        sys.exit(f"Air cannot exist at these conditions: drybulb = {self.db_C}C and relative humidity = {self.rh}")
     elif "enthalpy" in kwargs:
       self.set_h(kwargs["enthalpy"])
-    elif "dewp" in kwargs:
-      self.set_dewp(kwargs["dewp"])
+      h_check = psychrolib.GetMoistAirEnthalpy(self.db_C,self.get_hr())
+      if abs(h_check - self.h)/h_check > 0.01:
+        sys.exit(f"Air cannot exist at these conditions: drybulb = {self.db_C}C and enthalpy = {self.h}")
+    elif "dew_point" in kwargs:
+      self.set_dp(kwargs["dew_point"])
+      dp_check = psychrolib.GetTDewPointFromTWetBulb(self.db_C,self.get_wb_C(),self.p)
+      if abs(dp_check - self.dp_C)/dp_check > 0.01:
+        sys.exit(f"Air cannot exist at these conditions: drybulb = {self.db_C}C and dew point = {self.dp_C}C")
     else:
       sys.exit(f'Unknonw key word argument {kwargs}.')
 
@@ -38,13 +52,13 @@ class PsychState:
     self.wb_set = True
     return self.wb
 
-  def set_dewp(self, dewp):
-    self.dewp = dewp
-    self.dewp_C = to_u(self.dewp,"°C")
+  def set_dp(self, dp):
+    self.dp = dp
+    self.dp_C = to_u(self.dp,"°C")
     if not self.wb_set:
-      self.set_wb(fr_u(psychrolib.GetTWetBulbFromTDewPoint(self.db_C, self.dewp_C, self.p) ,"°C"))
-    self.dewp_set = True
-    return self.dewp
+      self.set_wb(fr_u(psychrolib.GetTWetBulbFromTDewPoint(self.db_C, self.dp_C, self.p) ,"°C"))
+    self.dp_set = True
+    return self.dp
 
   def set_rh(self, rh):
     self.rh = rh
@@ -79,15 +93,15 @@ class PsychState:
     else:
       sys.exit(f'Wetbulb not set')
 
-  def get_dewp(self):
-    if not self.dewp_set:
-      self.set_dewp(psychrolib.GetTDewPointFromTWetBulb(self.db_C,self.get_wb_C(),self.p))
-    return self.dewp
+  def get_dp(self):
+    if not self.dp_set:
+      self.set_dp(psychrolib.GetTDewPointFromTWetBulb(self.db_C,self.get_wb_C(),self.p))
+    return self.dp
 
-  def get_dewp_C(self):
-    if not self.dewp_set:
-      self.set_dewp(to_u(psychrolib.GetTDewPointFromTWetBulb(self.db_C,self.get_wb_C(),self.p),"°C"))
-    return self.dewp_C
+  def get_dp_C(self):
+    if not self.dp_set:
+      self.set_dp(to_u(psychrolib.GetTDewPointFromTWetBulb(self.db_C,self.get_wb_C(),self.p),"°C"))
+    return self.dp_C
 
   def set_rho(self, rho):
     self.rho = rho
@@ -118,6 +132,4 @@ class PsychState:
     else:
       return self.set_h(psychrolib.GetMoistAirEnthalpy(self.db_C,self.get_hr()))
 
-
-STANDARD_CONDITIONS = PsychState(drybulb=fr_u(70.0,"°F"),hum_rat=0.0)
-
+STANDARD_CONDITIONS = PsychState(drybulb=fr_u(70.0,"°F"),hum_rat=1e-07) # The min HR in psychrolib is 1e-07
