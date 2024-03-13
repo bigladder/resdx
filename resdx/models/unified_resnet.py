3f7c676e2963c663a33459708f0f36014821f5d8
Neal Kruis <neal.kruis@bigladdersoftware.com>
2024-03-13 15:40:26 -0600

    Add variable capacity calculations.

--- resdx/models/unified_resnet.py
+++ resdx/models/unified_resnet.py
@@ -7,6 +7,17 @@ from ..fan import ECMFlowFan, PSCFan
 
 
 class RESNETDXModel(DXModel):
+
+    def __init__(self):
+        super().__init__()
+        self.allowed_kwargs += [
+            "eer2",
+            "seer2",
+            "hspf2",
+            "rated_net_stead_state_heating_capacity_17",
+            "rated_net_total_cooling_min_capacity_ratio_95",
+        ]
+
     # Power and capacity
     def gross_cooling_power(self, conditions):
         return NRELDXModel.gross_cooling_power(self, conditions)
@@ -371,6 +382,249 @@ class RESNETDXModel(DXModel):
                     f"No default rated net total cooling capacities for systems with more than two speeds"
                 )
 
+    def set_variable_capacity_cooling_properties(self):
+        cooling_temperatures = [
+            fr_u(60, "degF"),
+            fr_u(82, "degF"),
+            fr_u(95, "degF"),
+            self.system.cooling_off_temperature,
+        ]
+
+        Tmin = 0
+        T82 = 1
+        T95 = 2
+        Tmax = 3
+
+        Qmin = 0
+        Qrated = 1
+        Qmax = 2
+
+        if "rated_net_total_cooling_min_capacity_ratio_95" in self.system.kwargs:
+            Qr95min = self.system.kwargs[
+                "rated_net_total_cooling_min_capacity_ratio_95"
+            ]
+        else:
+            Qr95min = (
+                0.474 - 0.119 * self.system.kwargs["seer2"] / self.system.kwargs["eer2"]
+            )
+
+        Qr95rated = 0.893
+        Qm95max = 0.955
+        Qm95min = 0.925
+        EIRr95rated = 0.879
+        EIRr95min = 0.710
+        EIRm95max = 1.368
+        EIRm95min = 1.318
+
+        Q = [[None] * (Qmax + 1) for _ in range(Tmax + 1)]  # Initialize dimensions
+        P = Q
+
+        # Net Total Capacity
+
+        # 95 F
+        Q[T95][Qrated] = self.system.rated_net_total_cooling_capacity
+        Q[T95][Qmax] = Q[T95][Qrated] / Qr95rated
+        Q[T95][Qmin] = Q[T95][Qmax] * Qr95min
+
+        # 82 F
+        Q[T82][Qmax] = Q[T95][Qmax] / Qm95max
+        Q[T82][Qmin] = Q[T95][Qmin] / Qm95min
+        Q[T82][Qrated] = Q[T82][Qmin] + (Q[T95][Qrated] - Q[T95][Qmin]) / (
+            Q[T95][Qmax] - Q[T95][Qrated]
+        ) * (
+            Q[T82][Qmax] - Q[T95][Qmin]
+        )  # Interpolate
+
+        # Tmin
+        Q[Tmin][Qmax] = extrapolate_below(Q, Qmax, cooling_temperatures)
+        Q[Tmin][Qrated] = extrapolate_below(Q, Qrated, cooling_temperatures)
+        Q[Tmin][Qmin] = extrapolate_below(Q, Qmin, cooling_temperatures)
+
+        # Tmax
+        Q[Tmax][Qmax] = extrapolate_above(Q, Qmax, cooling_temperatures)
+        Q[Tmax][Qrated] = extrapolate_above(Q, Qrated, cooling_temperatures)
+        Q[Tmax][Qmin] = extrapolate_above(Q, Qmin, cooling_temperatures)
+
+        # Net Power
+        Pr95rated = Qr95rated * EIRr95rated
+        Pr95min = Qr95min * EIRr95min
+        Pm95max = Qm95max * EIRm95max
+        Pm95min = Qm95min * EIRm95min
+
+        # 95 F
+        P[T95][Qrated] = Q[T95][Qrated] / fr_u(self.system.kwargs["eer2"], "Wh/Btu")
+        P[T95][Qmax] = P[T95][Qrated] / Pr95rated
+        P[T95][Qmin] = P[T95][Qmax] * Pr95min
+
+        # 82 F
+        P[T82][Qmax] = P[T95][Qmax] / Pm95max
+        P[T82][Qmin] = P[T95][Qmin] / Pm95min
+        P[T82][Qrated] = P[T82][Qmin] + (P[T95][Qrated] - P[T95][Qmin]) / (
+            P[T95][Qmax] - P[T95][Qrated]
+        ) * (
+            P[T82][Qmax] - P[T95][Qmin]
+        )  # Interpolate
+
+        # Tmin
+        P[Tmin][Qmax] = extrapolate_below(P, Qmax, cooling_temperatures)
+        P[Tmin][Qrated] = extrapolate_below(P, Qrated, cooling_temperatures)
+        P[Tmin][Qmin] = extrapolate_below(P, Qmin, cooling_temperatures)
+
+        # Tmax
+        P[Tmax][Qmax] = extrapolate_above(P, Qmax, cooling_temperatures)
+        P[Tmax][Qrated] = extrapolate_above(P, Qrated, cooling_temperatures)
+        P[Tmax][Qmin] = extrapolate_above(P, Qmin, cooling_temperatures)
+
+    def set_variable_capacity_heating_properties(self):
+        heating_temperatures = [
+            self.system.heating_off_temperature,
+            fr_u(5, "degF"),
+            fr_u(17, "degF"),
+            fr_u(47, "degF"),
+            fr_u(60, "degF"),
+        ]
+
+        Tmin = 0
+        T5 = 1
+        T17 = 2
+        T47 = 3
+        Tmax = 4
+
+        Qmin = 0
+        Qrated = 1
+        Qmax = 2
+
+        Qr47rated = 0.850
+        Qr47min = 0.260
+        Qr17rated = 0.817
+        Qr17min = 0.264
+        Qm5max = 0.897
+        Qr5rated = 0.974
+        Qr5min = 0.253
+        QmslopeLCTmax = -0.025
+        QmslopeLCTmin = -0.025
+        EIRr47rated = 0.904
+        EIRr47min = 0.738
+        EIRm17rated = 1.365
+        EIRr17rated = 0.882
+        EIRr17min = 0.844
+        EIRm5max = 1.118
+        EIRr5rated = 0.998
+        EIRr5min = 0.955
+        EIRmslopeLCTmax = 0.012
+        EIRmslopeLCTmin = 0.014
+
+        if "rated_net_stead_state_heating_capacity_17" in self.system.kwargs:
+            Qm17rated = (
+                self.system.kwargs["rated_net_stead_state_heating_capacity_17"]
+                / self.system.rated_net_total_cooling_capacity
+            )
+        else:
+            Qm17rated = 0.719
+
+        Q = [[None] * (Qmax + 1) for _ in range(Tmax + 1)]  # Initialize dimensions
+        P = Q
+
+        # Net Total Capacity
+
+        # 47 F
+        Q[T47][Qrated] = self.system.rated_net_total_cooling_capacity
+        Q[T47][Qmax] = Q[T47][Qrated] / Qr47rated
+        Q[T47][Qmin] = Q[T47][Qmax] * Qr47min
+
+        # 17 F
+        Q[T17][Qrated] = Q[T47][Qrated] * Qm17rated
+        Q[T17][Qmax] = Q[T17][Qrated] / Qr17rated
+        Q[T17][Qmin] = Q[T17][Qmax] * Qr17min
+
+        # 5 F
+        Q[T5][Qmax] = Q[T17][Qmax] * Qm5max
+        Q[T5][Qrated] = Q[T5][Qmax] * Qr5rated
+        Q[T5][Qmin] = Q[T5][Qmax] * Qr5min
+
+        QmLCTmax = 1 / (
+            1
+            - fr_u(QmslopeLCTmax, "1/degF")
+            * (heating_temperatures[T5] - heating_temperatures[Tmin])
+        )
+        QmLCTmin = 1 / (
+            1
+            - fr_u(QmslopeLCTmin, "1/degF")
+            * (heating_temperatures[T5] - heating_temperatures[Tmin])
+        )
+
+        # Tmin
+        Q[Tmin][Qmax] = Q[T5][Qmax] * QmLCTmax
+        Q[Tmin][Qmin] = Q[T5][Qmin] * QmLCTmin
+        Q[Tmin][Qrated] = Q[Tmin][Qmin] + (Q[T5][Qrated] - Q[T5][Qmin]) / (
+            Q[T5][Qmax] - Q[T5][Qrated]
+        ) * (
+            Q[Tmin][Qmax] - Q[T5][Qmin]
+        )  # Interpolate
+
+        # Tmax
+        Q[Tmax][Qmax] = Q[T47][Qmax]
+        Q[Tmax][Qrated] = Q[T47][Qrated]
+        Q[Tmax][Qmin] = Q[T47][Qmin]
+
+        # Net Power
+        if self.system.rated_net_heating_cop is None:
+            self.system.rated_net_heating_cop = (
+                2.222 + 0.123 * self.system.kwargs["hspf2"]
+            )
+
+        Pr47rated = Qr47rated * EIRr47rated
+        Pr47min = Qr47min * EIRr47min
+        Pm17rated = Qm17rated * EIRm17rated
+        Pr17rated = Qr17rated * EIRr17rated
+        Pr17min = Qr17min * EIRr17min
+        Pm5max = Qm5max * EIRm5max
+        Pr5rated = Qr5rated * EIRr5rated
+        Pr5min = Qr5min * EIRr5min
+
+        # 47 F
+        P[T47][Qrated] = Q[T47][Qrated] / self.system.rated_net_heating_cop
+        P[T47][Qmax] = P[T47][Qrated] / Pr47rated
+        P[T47][Qmin] = P[T47][Qmax] * Pr47min
+
+        # 17 F
+        P[T17][Qrated] = P[T47][Qrated] * Pm17rated
+        P[T17][Qmax] = P[T17][Qrated] / Pr17rated
+        P[T17][Qmin] = P[T17][Qmax] * Pr17min
+
+        # 5 F
+        P[T5][Qmax] = P[T17][Qmax] * Pm5max
+        P[T5][Qrated] = P[T5][Qmax] * Pr5rated
+        P[T5][Qmin] = P[T5][Qmax] * Pr5min
+
+        EIRmLCTmax = 1 / (
+            1
+            - fr_u(EIRmslopeLCTmax, "1/degF")
+            * (heating_temperatures[T5] - heating_temperatures[Tmin])
+        )
+        EIRmLCTmin = 1 / (
+            1
+            - fr_u(EIRmslopeLCTmin, "1/degF")
+            * (heating_temperatures[T5] - heating_temperatures[Tmin])
+        )
+
+        PmLCTmax = EIRmLCTmax * (Q[Tmin][Qmax] / Q[T5][Qmax])
+        PmLCTmin = EIRmLCTmin * (Q[Tmin][Qmin] / Q[T5][Qmin])
+
+        # Tmin
+        P[Tmin][Qmax] = P[T5][Qmax] * PmLCTmax
+        P[Tmin][Qmin] = P[T5][Qmin] * PmLCTmin
+        P[Tmin][Qrated] = P[Tmin][Qmin] + (P[T5][Qrated] - P[T5][Qmin]) / (
+            P[T5][Qmax] - P[T5][Qrated]
+        ) * (
+            P[Tmin][Qmax] - P[T5][Qmin]
+        )  # Interpolate
+
+        # Tmax
+        P[Tmax][Qmax] = P[T47][Qmax]
+        P[Tmax][Qrated] = P[T47][Qrated]
+        P[Tmax][Qmin] = P[T47][Qmin]
+
     def set_c_d_cooling(self, input):
         if self.system.input_seer is None:
             default = 0.25
@@ -430,3 +684,18 @@ class RESNETDXModel(DXModel):
         seer,
     ):  # Linear model fitted (R² = 0.994) based on data of the histrory of federal minimums (https://www.eia.gov/todayinenergy/detail.php?id=40232#).
         return seer * 0.3526 + 3.2627
+
+
+def extrapolate_below(ys, y_axis, xs):
+    return ys[1][y_axis] - (ys[2][y_axis] - ys[1][y_axis]) / (xs[2] - xs[1]) * (
+        xs[1] - xs[0]
+    )
+
+
+def extrapolate_above(ys, y_axis, xs):
+    i = len(xs) - 1
+    i_m1 = i - 1
+    i_m2 = i - 2
+    return ys[i_m1][y_axis] + (ys[i_m1][y_axis] - ys[i_m2][y_axis]) / (
+        xs[i_m1] - xs[i_m2]
+    ) * (xs[i] - xs[i_m1])
