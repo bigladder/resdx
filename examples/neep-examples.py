4b082cc0f3b5b52b9caa1440c428db157dcf1f65
Neal Kruis <neal.kruis@bigladdersoftware.com>
2024-05-01 12:40:31 -0600

    Remove old NEEP VCHP model.

--- examples/neep-examples.py
+++ examples/neep-examples.py
@@ -6,150 +6,106 @@ output_directory_path = "output"
 dx_units = {}
 
 # Mitsubishi Electric M-Series
-# MUZ-GL15NAH2 https://ashp.neep.org/#!/product/33396/7/25000///0
-# SEER 21.6, EER 13, HSPF 10.8
-cooling_data = resdx.VCHPDataPoints()
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(95.0, "°F"),
-        capacities=[fr_u(18200, "Btu/h"), fr_u(3100, "Btu/h")],
-        cops=[2.67, 4.33],
-    )
-)
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(82.0, "°F"),
-        capacities=[fr_u(20098, "Btu/h"), fr_u(3428, "Btu/h")],
-        cops=[3.27, 5.29],
-    )
-)
+# MUZ-GL15NAH2 AHRI Certification #: 202680596 https://ashp.neep.org/#!/product/34439/7/25000/95/7500/0///0
 
-heating_data = resdx.VCHPDataPoints()
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(47.0, "°F"),
-        capacities=[fr_u(20900, "Btu/h"), fr_u(4800, "Btu/h")],
-        cops=[3.05, 7.03],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(17.0, "°F"),
-        capacities=[fr_u(16400, "Btu/h"), fr_u(2150, "Btu/h")],
-        cops=[2.25, 1.91],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(5.0, "°F"),
-        capacities=[fr_u(14100, "Btu/h"), fr_u(2080, "Btu/h")],
-        cops=[2.43, 1.65],
-    )
-)
+cooling_capacities = [
+    [3428, None, 20098],  # 82
+    [3100, 14000, 18200],  # 95
+]
 
-dx_units["MUZ-GL15NAH2"] = resdx.make_vchp_unit(
-    cooling_data,
-    heating_data,
-    cooling_full_load_speed_ratio=14000.0 / 18200.0,
-    heating_full_load_speed_ratio=18000.0 / 20900.0,
+cooling_powers = [
+    [0.19, None, 1.8],  # 82
+    [0.21, 1.08, 2.0],  # 95
+]
+
+heating_capacities = [
+    [2080, None, 14100],  # 5
+    [2150, 12100, 16400],  # 17
+    [4800, 18000, 20900],  # 47
+]
+
+heating_powers = [
+    [0.24, None, 1.57],  # 5
+    [0.2, 1.6, 2.01],  # 17
+    [0.2, 1.6, 2.01],  # 47
+]
+
+
+dx_units["MUZ-GL15NAH2"] = resdx.DXUnit(
+    neep_data=resdx.models.neep_data.make_neep_model_data(
+        cooling_capacities, cooling_powers, heating_capacities, heating_powers
+    ),
+    input_seer=21.0,
+    input_eer=13.0,
+    input_hspf=11.0,
 )
 
 # Mitsubishi Electric M-Series
-# MUZ-FH18NAH2 https://ashp.neep.org/#!/product/25896/7/25000///0
-# SEER 21, EER 12.5, HSPF 11
-cooling_data = resdx.VCHPDataPoints()
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(95.0, "°F"),
-        capacities=[fr_u(21000, "Btu/h"), fr_u(6450, "Btu/h")],
-        cops=[2.77, 4.61],
-    )
-)
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(82.0, "°F"),
-        capacities=[fr_u(23184, "Btu/h"), fr_u(7126, "Btu/h")],
-        cops=[3.4, 5.64],
-    )
+# MUZ-FH18NAH2 AHRI Certification #: 201754303 https://ashp.neep.org/#!/product/25896/7/25000/95/7500/0///0
+
+cooling_capacities = [
+    [7126, None, 23184],  # 82
+    [6450, 17200, 21000],  # 95
+]
+
+cooling_powers = [
+    [0.37, None, 2.0],  # 82
+    [0.41, 1.38, 2.22],  # 95
+]
+
+heating_capacities = [
+    [3700, None, 20900],  # 5
+    [4300, 12480, 24300],  # 17
+    [7540, 20300, 30400],  # 47
+]
+
+heating_powers = [
+    [0.53, None, 2.98],  # 5
+    [0.6, 1.25, 3.39],  # 17
+    [0.85, 1.77, 3.4],  # 47
+]
+
+dx_units["MUZ-FH18NAH2"] = resdx.DXUnit(
+    neep_data=resdx.models.neep_data.make_neep_model_data(
+        cooling_capacities, cooling_powers, heating_capacities, heating_powers
+    ),
+    input_seer=21.0,
+    input_eer=12.5,
+    input_hspf=10.3,
 )
 
-heating_data = resdx.VCHPDataPoints()
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(47.0, "°F"),
-        capacities=[fr_u(30400, "Btu/h"), fr_u(7540, "Btu/h")],
-        cops=[2.62, 2.6],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(17.0, "°F"),
-        capacities=[fr_u(24300, "Btu/h"), fr_u(4300, "Btu/h")],
-        cops=[2.1, 2.1],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(5.0, "°F"),
-        capacities=[fr_u(20900, "Btu/h"), fr_u(3700, "Btu/h")],
-        cops=[2.06, 2.05],
-    )
-)
+# Mitsubishi Electric M-Series
+# PUZ-HA36NHA5 AHRI Certification #: 201754321 https://ashp.neep.org/#!/product/28981/7/25000/95/7500/0///0
 
-dx_units["MUZ-FH18NAH2"] = resdx.make_vchp_unit(
-    cooling_data,
-    heating_data,
-    cooling_full_load_speed_ratio=17200.0 / 21000.0,
-    heating_full_load_speed_ratio=20300.0 / 30400.0,
-)
+cooling_capacities = [
+    [17000, None, 37000],  # 82
+    [18000, 36000, 36000],  # 95
+]
 
-# Mitsubishi Electric P-Series
-# PUZ-HA36NHA5 https://ashp.neep.org/#!/product/28981/7/25000///0
-# SEER 17, EER 12.6, HSPF 10
-cooling_data = resdx.VCHPDataPoints()
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(95.0, "°F"),
-        capacities=[fr_u(36000, "Btu/h"), fr_u(18000, "Btu/h")],
-        cops=[3.7, 3.74],
-    )
-)
-cooling_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(82.0, "°F"),
-        capacities=[fr_u(37000, "Btu/h"), fr_u(17000, "Btu/h")],
-        cops=[4.93, 4.98],
-    )
-)
+cooling_powers = [
+    [1.0, None, 2.2],  # 82
+    [1.41, 2.85, 2.85],  # 95
+]
 
-heating_data = resdx.VCHPDataPoints()
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(47.0, "°F"),
-        capacities=[fr_u(40000, "Btu/h"), fr_u(19000, "Btu/h")],
-        cops=[3.13, 4.64],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(17.0, "°F"),
-        capacities=[fr_u(38000, "Btu/h"), fr_u(13000, "Btu/h")],
-        cops=[2.1, 3.34],
-    )
-)
-heating_data.append(
-    resdx.VCHPDataPoint(
-        drybulb=fr_u(5.0, "°F"),
-        capacities=[fr_u(38000, "Btu/h"), fr_u(8000, "Btu/h")],
-        cops=[1.92, 2.82],
-    )
-)
+heating_capacities = [
+    [8000, None, 38000],  # 5
+    [13000, 28000, 38000],  # 17
+    [19000, 38000, 40000],  # 47
+]
 
-dx_units["PUZ-HA36NHA5"] = resdx.make_vchp_unit(
-    cooling_data,
-    heating_data,
-    cooling_full_load_speed_ratio=36000.0 / 36000.0,
-    heating_full_load_speed_ratio=38000.0 / 40000.0,
+heating_powers = [
+    [0.83, None, 5.79],  # 5
+    [1.14, 3.59, 5.3],  # 17
+    [1.2, 3.13, 3.75],  # 47
+]
+
+dx_units["PUZ-HA36NHA5"] = resdx.DXUnit(
+    neep_data=resdx.models.neep_data.make_neep_model_data(
+        cooling_capacities, cooling_powers, heating_capacities, heating_powers
+    ),
+    input_seer=17.0,
+    input_eer=12.6,
+    input_hspf=10,
 )
 
 for dx_unit in dx_units:
