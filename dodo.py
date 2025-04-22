568a63d3111a48b80a8c81a61eee3ee8f28cc432
Neal Kruis <neal.kruis@bigladdersoftware.com>
2025-04-22 16:55:50 -0600

    Disable PDF generation.

--- dodo.py
+++ dodo.py
@@ -8,7 +8,7 @@ create_folder(OUTPUT_PATH)
 def task_test():
     """Performs tests"""
     return {
-        "actions": ["pytest -v test", 'echo "Tests completed"'],
+        "actions": ["pytest -v test"],
         "clean": True,
         "verbosity": 2,
     }
@@ -16,11 +16,10 @@ def task_test():
 
 def task_examples():
     """Run examples"""
-    print("Running examples...")
     for file_path in Path("examples").glob("*.py"):
         yield {
             "name": file_path.name,
-            "actions": ['echo "Example starting..."', f"python {file_path}"],
+            "actions": [f"python {file_path}"],
             "clean": True,
             "verbosity": 2,
         }
