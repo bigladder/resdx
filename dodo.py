5e49ba39be66bdbdf035029bf3171cd8cf14038a
Neal Kruis <neal.kruis@bigladdersoftware.com>
2025-04-22 16:49:21 -0600

    Add debug statements for Windows CI.

--- dodo.py
+++ dodo.py
@@ -8,7 +8,7 @@ create_folder(OUTPUT_PATH)
 def task_test():
     """Performs tests"""
     return {
-        "actions": ["pytest -v test"],
+        "actions": ["pytest -v test", 'echo "Tests completed"'],
         "clean": True,
         "verbosity": 2,
     }
@@ -16,10 +16,11 @@ def task_test():
 
 def task_examples():
     """Run examples"""
+    print("Running examples...")
     for file_path in Path("examples").glob("*.py"):
         yield {
             "name": file_path.name,
-            "actions": [f"python {file_path}"],
+            "actions": ['echo "Example starting..."', f"python {file_path}"],
             "clean": True,
             "verbosity": 2,
         }
