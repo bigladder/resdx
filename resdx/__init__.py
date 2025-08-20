b0ab516abdb2512116c8591d32918f988b0a831e
Neal Kruis <neal.kruis@bigladdersoftware.com>
2025-08-20 11:18:22 -0600

    Allow separate statistical sets for heating and cooling.

--- resdx/__init__.py
+++ resdx/__init__.py
@@ -2,8 +2,4 @@ from .cse import write_cse
 from .dx_unit import *
 from .fan import *
 from .idf import EnergyPlusSystemType, write_idf
-from .models import (
-    RESNETDXModel,
-    make_neep_model_data,
-    make_neep_statistical_model_data,
-)
+from .models import RESNETDXModel, make_neep_model_data, make_neep_statistical_model_data, StatisticalSet
--- resdx/models/__init__.py
+++ resdx/models/__init__.py
@@ -4,3 +4,4 @@ from .nrel import *
 from .tabular_data import make_neep_model_data, make_neep_statistical_model_data
 from .title24 import *
 from .unified_resnet import *
+from .statistical_set import StatisticalSet
