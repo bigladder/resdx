a62adbdad93ffa361522d21a51221cb04976837e
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2026-04-17 14:37:57 -0600

    Fix coolnig temps.

--- resdx/cse.py
+++ resdx/cse.py
@@ -300,7 +300,7 @@ def write_cse(
         if unit.net_tabular_data is not None:
             cooling_outdoor_dry_bulbs = to_u(unit.net_tabular_data.cooling_capacities.temperatures, "°F")
             if max(unit.net_tabular_data.cooling_capacities.temperatures) < fr_u(125, "°F"):
-                cooling_outdoor_dry_bulbs = to_u(unit.net_tabular_data.cooling_capacities.temperatures, "°F") + [125.0]
+                cooling_outdoor_dry_bulbs += [125.0]
     cooling_speeds: list[int]
     if unit.staging_type == StagingType.VARIABLE_SPEED:
         cooling_speeds = [unit.cooling_low_speed, unit.cooling_full_load_speed, unit.cooling_boost_speed]
